3eb7c9f268536e72d74971190f137c9645c3784a
Ramazan BILEN <ramazan10bilen@gmail.com>
2021-04-20 11:05:11 +0300

    fatal

--- edabit/01-very-easy/how-edabit-works.py
+++ edabit/01-very-easy/how-edabit-works.py
@@ -1,24 +1,2 @@
-"""
-
-This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
-
-def hello():
-All you have to do is type return "hello edabit.com" on the second line and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
-
-Notes
-The returned string must be in all lowercase letters.
-If you get stuck on a challenge, find help in the Resources tab.
-If you're really stuck, unlock solutions in the Solutions tab.
-
-"""
-from edabit.Test import Test
 
 
-def hello():
-    """
-    Summary: This method returns a string
-    """
-    return "hello edabit.com"
-
-
-Test.assert_equals(hello(), "hello edabit.com")
