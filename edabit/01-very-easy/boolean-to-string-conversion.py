1c57a04b4b4b7e292d2a66fdb54bc670ba45c58d
Ramazan BILEN <ramazan10bilen@gmail.com>
2021-04-25 15:42:58 +0300

    boolean-to-string-conversion

---
+++ edabit/01-very-easy/boolean-to-string-conversion.py
@@ -0,0 +1,26 @@
+"""
+Boolean to String Conversion
+Create a function that takes a boolean variable flag and returns it as a string.
+
+Examples
+bool_to_string(True) ➞ "True"
+
+bool_to_string(False) ➞ "False"
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+"""
+
+from edabit.Test import Test
+
+
+def bool_to_string(flag):
+    if flag == True:
+        return "True"
+    else:
+        return "False"
+
+if __name__ == '__main__':
+    Test.assert_equals(bool_to_string(True), "True")
+    Test.assert_equals(bool_to_string(False), "False")
