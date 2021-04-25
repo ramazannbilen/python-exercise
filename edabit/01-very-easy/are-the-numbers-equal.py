95a6a2b74b238cfa485f7d983b0ff9df7ff88239
Ramazan BILEN <ramazan10bilen@gmail.com>
2021-04-25 20:58:52 +0300

    are-the-numbers-equal

---
+++ edabit/01-very-easy/are-the-numbers-equal.py
@@ -0,0 +1,30 @@
+"""
+Are the Numbers Equal?
+Create a function that returns True when num1 is equal to num2; otherwise return False.
+
+Examples
+is_same_num(4, 8) ➞ False
+
+is_same_num(2, 2) ➞  True
+
+is_same_num(2, "2") ➞ False
+Notes
+Don't forget to return the result.
+
+"""
+
+from edabit.Test import Test
+
+
+def is_same_num(num1, num2):
+    if num1 == num2:
+        return True
+    else:
+        return False
+    
+
+if __name__ == '__main__':
+    Test.assert_equals(is_same_num(4, 8), False)
+    Test.assert_equals(is_same_num(2, 2), True)
+    Test.assert_equals(is_same_num(0, 6), False)
+    Test.assert_equals(is_same_num(2, "2"), False)
