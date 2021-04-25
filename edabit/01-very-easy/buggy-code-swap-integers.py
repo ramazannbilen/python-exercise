f0b50b1df597ed13b640a20cb1633ee44f8dfa38
Ramazan BILEN <ramazan10bilen@gmail.com>
2021-04-25 21:44:51 +0300

    buggy-code-swap-integers

---
+++ edabit/01-very-easy/buggy-code-swap-integers.py
@@ -0,0 +1,32 @@
+"""
+Buggy Code (Part 7)
+Mubashir wants to swap two given numbers!
+
+It is not returning the right values. Can you help him fix it?
+
+a = 100
+b = 200
+a, b = swap(a, b)
+print(a, b) # Should print out "200, 100", but the function prints out "100, 100"
+Examples
+swap(100, 200) ➞ [200, 100]
+
+swap(44, 33) ➞ [33, 44]
+
+swap(21, 12) ➞ [12, 21]
+"""
+
+from edabit.Test import Test
+
+
+def swap(a, b):
+    temp = a
+    a = b
+    b = temp
+    return [a, b]
+
+if __name__ == '__main__':
+    Test.assert_equals(swap(100, 200), [200, 100])
+    Test.assert_equals(swap(44, 33), [33, 44])
+    Test.assert_equals(swap(21, 12), [12, 21])
+    Test.assert_equals(swap(10, 20), [20, 10])
