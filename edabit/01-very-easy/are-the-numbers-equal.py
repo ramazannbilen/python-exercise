608d4e0bdcd7c35903b3ac0ad690b269308bc873
Ramazan BILEN <ramazan10bilen@gmail.com>
2021-05-30 15:30:39 +0300

    count-ones-in-binary-representation-of-an-integer

--- edabit/01-very-easy/are-the-numbers-equal.py
+++ edabit/01-very-easy/are-the-numbers-equal.py
@@ -10,7 +10,6 @@ is_same_num(2, 2) ➞  True
 is_same_num(2, "2") ➞ False
 Notes
 Don't forget to return the result.
-
 """
 
 from edabit.Test import Test
@@ -21,7 +20,7 @@ def is_same_num(num1, num2):
         return True
     else:
         return False
-    
+
 
 if __name__ == '__main__':
     Test.assert_equals(is_same_num(4, 8), False)
