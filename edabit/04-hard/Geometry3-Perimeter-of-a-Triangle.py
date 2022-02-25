57fc3dd93dac97ad18c97fbb53158532a11f2060
ramazan.bilen <ramazan10bilen@gmail.com>
2022-02-25 22:58:45 +0300

    Geometry3-Perimeter-of-a-Triangle.py

---
+++ edabit/04-hard/Geometry3-Perimeter-of-a-Triangle.py
@@ -0,0 +1,30 @@
+"""
+Geometry 3: Perimeter of a Triangle
+Write a function that takes the coordinates of three points in the form of a 2d array and returns the perimeter of the triangle. The given points are the vertices of a triangle on a two-dimensional plane.
+
+Examples
+perimeter([[15, 7], [5, 22], [11, 1]]) ➞ 47.08
+
+perimeter([[0, 0], [0, 1], [1, 0]]) ➞ 3.42
+
+perimeter([[-10, -10], [10, 10 ], [-10, 10]]) ➞ 68.28
+Notes
+The given points always create a triangle.
+The numbers in the argument array can be positive or negative.
+Output should have 2 decimal places
+This challenge is easier than it looks.
+"""
+
+from edabit.Test import Test
+
+
+def perimeter(lst):
+    return
+
+Test.assert_equals(perimeter([[0, 0], [1, 0], [0, 1]]), 3.41)
+Test.assert_equals(perimeter([[15, 7], [5, 22], [11, 1]]), 47.08)
+Test.assert_equals(perimeter([[7, 6], [0, 11], [0, -3]]), 34.00)
+Test.assert_equals(perimeter([[-10, -10], [10, 10], [-10, 10]]), 68.28)
+Test.assert_equals(perimeter([[3, 4], [4, 3], [4, 5]]), 4.83)
+Test.assert_equals(perimeter([[-10, -20], [-30, -40], [-50, -60]]), 113.14)
+Test.assert_equals(perimeter([[382, 894], [389, 312], [500, 993]]), 1426.06)
